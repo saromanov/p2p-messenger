8d4d485623d661ac22d359b7155acb9385c2265a
Sergey <xxsmotur@gmail.com>
2019-03-09 00:05:50 +0500

    Making of peers handling

--- internal/peer/peers.go
+++ internal/peer/peers.go
@@ -1,17 +1,40 @@
 package peer
 
-import "sync"
+import (
+	"errors"
+	"sync"
+)
+
+var errPeerNotFound = errors.New("peeer is not found")
 
 // Peers defines struct for store of map of peers
 type Peers struct {
-	mu    *sync.Mutex
+	mu    *sync.RWMutex
 	peers map[string]*Peer
 }
 
 // newPeers inits struct of peers
 func newPeers() *Peers {
 	return &Peers{
-		mu:    &sync.Mutex{},
+		mu:    &sync.RWMutex{},
 		peers: make(map[string]*Peer),
 	}
+}
+
+// Add provides inserting of the new peer
+func (p *Peers) Add(name string, peer *Peer) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	p.peers[name] = peer
+}
+
+// Get provides getting of the peer by the name
+func (p *Peers) Get(name string) (*Peer, error) {
+	p.mu.RLock()
+	defer p.mu.RUnlock()
+	peer, ok := p.peers[name]
+	if !ok {
+		return nil, errPeerNotFound
+	}
+	return peer, nil
 }
