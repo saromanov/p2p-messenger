d1f3e6609f7f585a4da2a9a38e843ff5c069fa8f
Sergey <xxsmotur@gmail.com>
2019-03-08 23:54:01 +0500

    Add Peer package

---
+++ internal/peer/peer.go
@@ -0,0 +1,23 @@
+// Package peer defines peer for the messenger
+package peer
+
+import (
+	"net"
+
+	"golang.org/x/crypto/ed25519"
+)
+
+// Peer defines peer for the messagener
+type Peer struct {
+	Name       string
+	PublicKey  ed25519.PublicKey
+	Connection *net.Conn
+}
+
+// New creates a new peer
+func New(c *net.Conn) (*Peer, error) {
+	return &Peer{
+		Name:       "123",
+		Connection: c,
+	}, nil
+}
